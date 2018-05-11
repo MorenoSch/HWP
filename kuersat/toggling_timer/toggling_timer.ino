269d9c9d37caa71fa9195c180b221e629722e92f
kuersatakmaz <akmazk@tf.uni-freiburg.de>
2018-05-11 12:05:19 +0200

    modified

--- kuersat/toggling_timer/toggling_timer.ino
+++ kuersat/toggling_timer/toggling_timer.ino
@@ -1,11 +1,11 @@
 // Copyright 2015: Jan Burchard
 // HWP Code: Low Level Pin Toggling with timer
-
+volatile uint32_t tCount;
 void setup() {
-  
+  Serial.begin(115200);  
   // pin as output
   pinMode(12, OUTPUT);
-  
+  tCount = 0;
   // how about a tone with 400 Hz
   // -> counter has to overflow 800x /second
   // set divider at 64
@@ -19,28 +19,36 @@ void setup() {
   TCCR2A = 0;     
   TCCR2B = 0;    
   
-  // set clock prescaler: 256
+  //reset control register for timer 1
+  TCCR1A = 0;
+  TCCR1b = 0;
+
+  // set clock prescaler timer 2: 64
   TCCR2B |= (1 << CS22);
-  // set mode (CTC)
+  
+  // set clock prescaler timer 1  : 64
+  TCCR1B |= (1 << CS22)
+
+
+  // set mode (CTC) for timer 1 and 2
   TCCR2A |= (1 << WGM21);
+  TCCR1A |= (1 << WGM21);
     
-  // set output compare register A
+  // set output compare register A for timer1 and 2
   OCR2A = 39;
-    
+  OCR1A = 125 
   // enable interrupt
-  TIMSK2 |= (1 << OCIE2A);    
+  TIMSK2 |= (1 << OCIE2A);
+  TIMSK1 |= (1 << OCIE1A);
   
   // eable all interrupts
   sei();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1000);
-  Pin12freq(800);
-  delay(1000);
-  Pin12freq(100);
-  
+  if(tCount %1000 == 0){
+    Serial.println(tcount/1000);
+  }
 }
 
 // interrupt service routine for timer 2 compare match
@@ -50,6 +58,12 @@ ISR(TIMER2_COMPA_vect) {
   PINB |= (1 << 4);
 }
 
+// timer one compare match
+ISR(TIMER1_COMPA_vect) {
+  
+  // toggle pin 13
+  tCount += 1;
+}
 
 void Pin12freq(uint16_t freq){
   uint16_t interrupt = 125000/(freq * 2);
