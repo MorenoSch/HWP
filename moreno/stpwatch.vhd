77b20fac6ded7c854630be05372e98d6aa940b81
MorenoSch <37292077+MorenoSch@users.noreply.github.com>
2018-06-12 12:39:22 +0200

    yay

--- moreno/stpwatch.vhd
+++ moreno/stpwatch.vhd
@@ -13,38 +13,30 @@ clk_50 	: in std_logic;
 startstop: in std_logic;
 reset 	: in std_logic;
 
-counting : out std_logic_vector(5 downto 0));
+counting : out unsigned(5 downto 0));
 end stpwatch;
 
 architecture behavior of stpwatch is
 signal count 	: unsigned(5 downto 0);
 signal counter : unsigned(31 downto 0);
 signal strstp  : boolean := true;
-signal rst		: boolean := false;
 
 begin
 process(clk_50)
-begin 
-		if(rising_edge(startstop)) then
-			strstp <= not strstp;
-		end if;
-		
-		if(rising_edge(reset)) then
-			rst <= not rst;
-		end if;
-	if (strstp) then
-		if(rising_edge(clk_50)) then
-			counter <= counter + 1;
-		end if;
-		if( counter >= 62000000 or rst) then
-			counter <= "00000000000000000000000000000000";
-			if(not rst) then
-				count <= count + 1;
-			else
-				count <= "000000";
-			end if;
-		end if;
+begin
+	if ( rising_edge(startstop)) then
+		strstp <= not strstp;
+	end if;
+	if (rising_edge(reset)) then 
+		count <= "000000";
+	end if;
+	if (rising_edge(clk_50) and strstp) then
+		counter <= counter + 1; 
+	end if;
+	if( counter >= 55000000) then
+		counter <= "00000000000000000000000000000000";
+		count <= count + 1;
 	end if;
 end process;
-counting <= std_logic_vector(count);
+counting <= count;
 end behavior;
